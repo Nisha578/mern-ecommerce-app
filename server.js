ee937dd75ae461e37dff9d020cc5fa35e55279a9
Nisha Yadav <vatsnishaa@gmail.com>
2023-12-12 01:24:39 -0800

    Update

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 // configure env
 dotenv.config();
 
 // database config
 connectDb();
+
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
